1773645fcd7af5ec21021d2efe1587ca5c348174
Sergey Fesyura <sergey.fesyura@dune-hd.com>
2014-05-06 13:11:41 +0300

    utins fix

--- server/src/utils/my_proplists.erl
+++ server/src/utils/my_proplists.erl
@@ -2,6 +2,6 @@
 -export(delete_several/2).
 
 delete_several([Key | Tail], Proplist) -> 
-	proplists:delete(Key, delete_several(Tail,Proplist)).
+	proplists:delete(Key, delete_several(Tail,Proplist));
 delete_several([], Proplist) -> 
 	Proplist.
