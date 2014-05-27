3ce331d25f2fdf97416b025ed517f2c690e313cd
Sergey Fesyura <sergey.fesyura@dune-hd.com>
2014-05-27 17:54:23 +0300

    bot started

--- bot/src/Main.hs
+++ bot/src/Main.hs
@@ -1,23 +1,47 @@
 module Main ( main ) where
 
 import qualified Network.WebSockets as WS
+import Control.Concurrent (forkIO)
+import Control.Monad (forever, unless)
+import Control.Monad.IO.Class (liftIO)
+import qualified Data.ByteString.Lazy.Char8 as BL
+import qualified Data.ByteString.Char8 as B
+import Data.Aeson
+import Control.Applicative ((<$>), (<*>), empty)
+import qualified Data.Text as Text
+
+data Action = Action { action :: String, value :: String}
+	 deriving (Show)
+
+instance ToJSON Action where
+  toJSON (Action xV yV) = object [
+    x .= xV,
+    y .= yV ] where
+    x = Text.pack("x")
+    y = Text.pack("y")
+instance FromJSON Action where
+  parseJSON (Object v) = Action <$>
+                         v .: "action" <*>
+                         v .: "value"
+  parseJSON _ = empty
 
 main :: IO ()
-main = WS.runClient "2048.php.poltava.ua" 8080 "/" app
+main = WS.runClient "127.0.0.1" 8081 "/websocket" app
 
 app :: WS.ClientApp ()
 app conn = do
     putStrLn "Connected!"
 
     -- Fork a thread that writes WS data to stdout
-    _ <- forkIO $ forever $ do
-        msg <- WS.receiveData conn
-        liftIO $ T.putStrLn msg
+    _ <- forkIO $ forever $ handler
 
-    -- Read from stdin and write to WS
-    let loop = do
-            line <- T.getLine
-            unless (T.null line) $ WS.sendTextData conn line >> loop
+    let start = Action "start"
+    print start
+    WS.sendTextData conn $ encode . start
 
-    loop
-    WS.sendClose conn ("Bye!" :: Text)
+    WS.sendClose conn
+
+handler :: WS.ClientApp () -> IO ()
+handler conn = do
+	msg <- WS.receiveData conn
+	liftIO $ B.putStrLn msg
