d53b7be563f379c45ea61e08708ee77175edf268
peinguin <pingvein@gmail.com>
2014-05-28 19:29:13 +0300

    tmp

--- server/src/game/game.erl
+++ server/src/game/game.erl
@@ -238,8 +238,7 @@ move(Vector, State) ->
                 true -> %return state otherwise
                     {struct,PreparedJsonData}
             end
-    end
-.
+    end.
 
 movesAvailable(_) ->
     true.
